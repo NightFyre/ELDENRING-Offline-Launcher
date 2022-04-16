0bbc67250e54e0de65c60df19695e61118043ed1
NightFyre <80198020+xCENTx@users.noreply.github.com>
2022-04-16 06:47:19 -0400

    Delete Console Launcher

--- src/injector.h
+++
@@ -1,8 +0,0 @@
-//	Load Library EX injector
-#pragma once
-#include <Windows.h>
-#include <TlHelp32.h>
-#include "resource.h"
-
-//	Credit : GuidedHacking | Broihon | GHInjector
-bool Inject(HANDLE Process);
